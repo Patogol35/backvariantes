d00e40ad44b90e0396b3755b4ee6b24509e50984
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-04-19 11:29:00 -0500

    Update filters.py

--- tienda/filters.py
+++ tienda/filters.py
@@ -1,15 +1,36 @@
 import django_filters
 from .models import Producto
 
-class ProductoFilter(django_filters.FilterSet):
-    precio_min = django_filters.NumberFilter(field_name='precio', lookup_expr='gte')
-    precio_max = django_filters.NumberFilter(field_name='precio', lookup_expr='lte')
 
+class ProductoFilter(django_filters.FilterSet):
+
+    # 🔥 FILTRO POR PRECIO (VARIANTES)
+    precio_min = django_filters.NumberFilter(
+        field_name='variantes__precio',
+        lookup_expr='gte'
+    )
+    precio_max = django_filters.NumberFilter(
+        field_name='variantes__precio',
+        lookup_expr='lte'
+    )
+
+    # 📂 CATEGORÍA
     categoria = django_filters.NumberFilter(field_name='categoria__id')
 
-    # 🔥 FILTRO POR TALLA
-    talla = django_filters.CharFilter(field_name='variantes__talla')
+    # 🎯 VARIANTES
+    talla = django_filters.CharFilter(field_name='variantes__talla', lookup_expr='iexact')
+    color = django_filters.CharFilter(field_name='variantes__color', lookup_expr='iexact')
+    capacidad = django_filters.CharFilter(field_name='variantes__capacidad', lookup_expr='iexact')
+    modelo = django_filters.CharFilter(field_name='variantes__modelo', lookup_expr='iexact')
 
     class Meta:
         model = Producto
-        fields = ['precio_min', 'precio_max', 'categoria', 'talla']
+        fields = [
+            'precio_min',
+            'precio_max',
+            'categoria',
+            'talla',
+            'color',
+            'capacidad',
+            'modelo'
+        ]
