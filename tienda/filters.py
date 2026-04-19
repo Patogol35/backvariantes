13466ba70e02d41e056236937090b84e164b37f6
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-04-19 10:09:08 -0500

    Update filters.py

--- tienda/filters.py
+++ tienda/filters.py
@@ -7,28 +7,10 @@ class ProductoFilter(django_filters.FilterSet):
     precio_max = django_filters.NumberFilter(field_name='precio', lookup_expr='lte')
     categoria = django_filters.NumberFilter(field_name='categoria__id')
 
-    # 🔥 FILTROS DINÁMICOS
-    talla = django_filters.CharFilter(method='filter_talla')
-    color = django_filters.CharFilter(method='filter_color')
+    # ✅ FILTROS REALES
+    talla = django_filters.CharFilter(field_name='variantes__talla', lookup_expr='iexact')
+    color = django_filters.CharFilter(field_name='variantes__color', lookup_expr='iexact')
 
     class Meta:
         model = Producto
         fields = ['precio_min', 'precio_max', 'categoria', 'talla', 'color']
-
-    # ------------------------------------------------------------
-    # 🔥 FILTRO TALLA (DINÁMICO)
-    # ------------------------------------------------------------
-    def filter_talla(self, queryset, name, value):
-        return queryset.filter(
-            variantes__atributos__tipo__nombre__iexact='talla',
-            variantes__atributos__valor__iexact=value
-        ).distinct()
-
-    # ------------------------------------------------------------
-    # 🔥 FILTRO COLOR (DINÁMICO)
-    # ------------------------------------------------------------
-    def filter_color(self, queryset, name, value):
-        return queryset.filter(
-            variantes__atributos__tipo__nombre__iexact='color',
-            variantes__atributos__valor__iexact=value
-        ).distinct()
