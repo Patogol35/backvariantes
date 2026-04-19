8c4a9f078216113251e5138a7e58b8bdc0d16fd2
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-04-19 10:31:02 -0500

    Update filters.py

--- tienda/filters.py
+++ tienda/filters.py
@@ -1,16 +1,15 @@
 import django_filters
 from .models import Producto
 
-
 class ProductoFilter(django_filters.FilterSet):
     precio_min = django_filters.NumberFilter(field_name='precio', lookup_expr='gte')
     precio_max = django_filters.NumberFilter(field_name='precio', lookup_expr='lte')
+
     categoria = django_filters.NumberFilter(field_name='categoria__id')
 
-    # ✅ FILTROS REALES
-    talla = django_filters.CharFilter(field_name='variantes__talla', lookup_expr='iexact')
-    color = django_filters.CharFilter(field_name='variantes__color', lookup_expr='iexact')
+    # 🔥 FILTRO POR TALLA
+    talla = django_filters.CharFilter(field_name='variantes__talla')
 
     class Meta:
         model = Producto
-        fields = ['precio_min', 'precio_max', 'categoria', 'talla', 'color']
+        fields = ['precio_min', 'precio_max', 'categoria', 'talla']
